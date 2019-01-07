d796b3fc2cbb3ea0e1c06d2b68a650f9037a4acd
reisman <30975669+reisman@users.noreply.github.com>
2019-01-07 18:43:12 +0100

    initial checkin

---
+++ main.js
@@ -0,0 +1,11 @@
+const { app, BrowserWindow } = require('electron');
+
+app.on('ready', function(){
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        title: 'Image Viewer'
+    });
+    win.setMenu(null);
+    win.loadFile('index.html');
+});
